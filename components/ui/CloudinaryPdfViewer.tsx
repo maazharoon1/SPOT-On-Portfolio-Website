6f5aa2bc2aa53336b77d9b59209b1468852246c7
maazharoon1 <maazharoon900@gmail.com>
2026-08-15 05:39:55 +0500

    tyoesafety

--- components/ui/CloudinaryPdfViewer.tsx
+++ components/ui/CloudinaryPdfViewer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-export default function CloudinaryPDFViewer({ publicId} ) {
+export default function CloudinaryPDFViewer({ publicId}:{publicId :string} ) {
   
   return (
    <div>
