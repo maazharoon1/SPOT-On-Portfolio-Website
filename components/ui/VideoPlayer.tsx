f28f76cb541448b678afc1617751cdd931552bd0
maazharoon1 <maazharoon900@gmail.com>
2026-08-18 00:20:41 +0500

    v

--- components/ui/VideoPlayer.tsx
+++ components/ui/VideoPlayer.tsx
@@ -1,36 +1,44 @@
 "use client";
 
-import { base } from "motion/react-client";
 import { CldVideoPlayer } from "next-cloudinary";
 import "next-cloudinary/dist/cld-video-player.css";
 
-export default function PortfolioVideo({ id,mainImage }: { id: string , mainImage :string }) {
+interface PortfolioVideoProps {
+  id: string;
+  mainImage: string;
+}
+
+export default function PortfolioVideo({
+  id,
+  mainImage,
+}: PortfolioVideoProps) {
   return (
-    <div className="flex h-full w-full items-center justify-center ">
+    <div className="flex h-full w-full items-center justify-center">
       <div
         className="
           relative
+          aspect-video
           w-full
           max-w-[95%]
           overflow-hidden
           rounded-xl
           md:max-h-[92%]
           md:max-w-[92%]
-          aspect-video
         "
       >
         <CldVideoPlayer
-        id={id}
-        poster={mainImage}
+          id={id}
           src={id}
+          poster={mainImage}
           width={1280}
           height={720}
           className="m-0! h-full! w-full!"
           colors={{
-            base:"#fff",
-            accent:"#3b1656",
+            base: "#fff",
+            accent: "#3b1656",
           }}
-            transformation={{
+          transformation={{
+            videoCodec: "auto",
             quality: "auto",
           }}
         />
